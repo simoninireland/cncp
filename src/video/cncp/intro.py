97198e96d2e1242f34968cb1da62612338475bd4
Simon Dobson <simon.dobson@computer.org>
2022-03-17 14:40:42 +0000

    Minor changes

--- src/video/cncp/intro.py
+++ src/video/cncp/intro.py
@@ -22,26 +22,42 @@ from manim import *
 
 class Intro(Scene):
 
+    def __init__(self, title = 'An example episode title', subtitle = None):
+        super().__init__()
+        self._title = title
+        self._subtitle = subtitle
+
     def construct(self):
         # logo
         logo = ImageMobject('./logo.png')
         logo.height=2
         logo.shift(3 * LEFT)
 
-        # text
-        title = Text('Complex networks, complex processes', font_size=26)
-        title.next_to(logo, RIGHT)
-        subtitle = Text('A network science miscellany', slant=ITALIC, font_size=20)
-        subtitle.align_to(title, LEFT).shift(0.4 * DOWN)
-        textGroup = VGroup(title, subtitle)
-        textGroup.next_to(logo, RIGHT)
+        # series title
+        seriesTitle = Text('Complex networks, complex processes', font_size=26)
+        seriesTitle.next_to(logo, RIGHT)
+        seriesSubtitle = Text('A network science miscellany', slant=ITALIC, font_size=20)
+        seriesSubtitle.align_to(seriesTitle, LEFT).shift(0.4 * DOWN)
+        seriesGroup = VGroup(seriesTitle, seriesSubtitle)
+        seriesGroup.next_to(logo, RIGHT)
+
+        # episode title
+        title = Text(self._title, font_size=26)
+        if self._subtitle is not None:
+            # we have a sub-title, add it to a title group
+            subtitle = Text(self._subtitle, slant=ITALIC, font_size=20)
+            subtitle.align_to(title, LEFT).shift(0.4 * DOWN)
+            title = VGroup(title, subtitle)
 
         self.play(FadeIn(logo))
-        self.play(Create(textGroup))
+        self.play(Create(seriesGroup))
         self.wait(2)
 
         self.play(logo.animate.scale(0.5).shift(3 * LEFT + 3 * UP),
-                  textGroup.animate.scale(0.5).shift(5 * LEFT + 3 * UP))
+                  seriesGroup.animate.scale(0.5).shift(5 * LEFT + 3 * UP))
+
+        self.play(Create(title))
         self.wait(2)
 
-        self.play(FadeOut(logo), FadeOut(textGroup))
+        self.play(FadeOut(logo), FadeOut(seriesGroup), FadeOut(title))
+        self.wait(3)
